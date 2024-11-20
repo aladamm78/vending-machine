9e6d4d77af1555396cdca81f80355768e043120f
aladamm78 <aladamm78@gmail.com>
2024-11-20 16:13:47 -0500

    Final Submission

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './App.css';
-    
+import './App.css'; // Adjust if you're using a specific CSS file
 
 function VendingMachine() {
   const backgroundStyle = {
-    backgroundImage: 'url(https://wallpapers.com/images/hd/rocket-vending-machine-9qrtyaz9czfq4j6l.jpg)',
+    backgroundImage: 'url(https://wallpapers.com/images/hd/rocket-vending-machine-9qrtyaz9czfq4j6l.jpg)', // Replace with actual image URL
   };
 
   return (
     <div style={backgroundStyle} className="page">
-      <h1>Welcome to the Vending Machine</h1>
-      <ul>
-        <li><Link to="/snack1" className="link">Lay's Chips</Link></li>
-        <li><Link to="/snack2" className="link">Skittles Tropical</Link></li>
-        <li><Link to="/snack3" className="link">Gatorade Fierce Strawberry</Link></li>
-      </ul>
+      <h1 className="page-title">Welcome to the Vending Machine</h1>
+      <div className="snack-links">
+        <Link to="/snack1" className="snack-link">Lay's Chips</Link>
+        <Link to="/snack2" className="snack-link">Skittles Tropical</Link>
+        <Link to="/snack3" className="snack-link">Gatorade Fierce Strawberry</Link>
+      </div>
     </div>
   );
 }
